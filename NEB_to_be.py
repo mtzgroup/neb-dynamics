fixed the gradient convergence problem

NEB_to_be.py
@@ -55,7 +55,7 @@ def toy_grad(x, y):
         return [0, 0]
     dx = -2 * x
     dy = -2 * y
-    return dx, dy
+    return np.array([dx, dy])
 
 
 def toy_potential_2(x, y):
@@ -66,7 +66,7 @@ def toy_potential_2(x, y):
 def toy_grad_2(x, y):
     dx = 2 * (x**2 + y - 11) * (2 * x) + 2 * (x + y**2 - 7)
     dy = 2 * (x**2 + y - 11) + 2 * (x + y**2 - 7) * (2 * y)
-    return dx, dy
+    return np.array([dx, dy])
 
 
 # -
@@ -89,23 +89,21 @@ def toy_grad_3(x, y):
         / (alpha * np.exp(np.abs(beta)))
     )
 
-    return dx, dy
+    return np.array([dx, dy])
 
 
-def spring_grad_neb(x, y, neighs, k=0.1, ideal_distance=0.5, en_func=toy_potential, grad_func=toy_grad):
+def spring_grad_neb(view, k=0.1, ideal_distance=0.5, en_func=toy_potential, grad_func=toy_grad):
 
     # pe_grad = 0
-    # print(f"{neighs=} // {len(neighs)}")
-    if len(neighs) == 1:
-        vec_tan_path = neighs[0] - np.array([x, y])
+    neighs = view[[0,2]]
+    x,y = view[1]
 
-    elif len(neighs) == 2:
-        vec_tan_path = np.array(neighs[1]) - np.array(neighs[0])
-    else:
-        raise ValueError("Wtf are you doing.")
+    
+    vec_tan_path = neighs[1] - neighs[0]
+
 
     unit_tan_path = vec_tan_path / np.linalg.norm(vec_tan_path)
-    # print(f"{unit_tan_path=}")
+    
 
     pe_grad = grad_func(x, y)
     pe_grad_nudged_const = np.dot(pe_grad, unit_tan_path)
@@ -141,22 +139,17 @@ def spring_grad_neb(x, y, neighs, k=0.1, ideal_distance=0.5, en_func=toy_potenti
     return tot_grads_neighs - pe_grad_nudged
 
 
-def spring_grad(x, y, neighs, k=0.1, ideal_distance=0.5, en_func=toy_potential, grad_func=toy_grad):
+def spring_grad(view, k=0.1, ideal_distance=0.5, en_func=toy_potential, grad_func=toy_grad):
+
 
     # pe_grad = 0
-    # print(f"{neighs=} // {len(neighs)}")
-    if len(neighs) == 1:
-        vec_tan_path = neighs[0] - np.array([x, y])
+    neighs = view[[0,2]]
+    x,y = view[1]
 
-    elif len(neighs) == 2:
-        vec_tan_path = np.array(neighs[1]) - np.array(neighs[0])
-    else:
-        raise ValueError("Wtf are you doing.")
 
-    unit_tan_path = vec_tan_path / np.linalg.norm(vec_tan_path)
-    # print(f"{unit_tan_path=}")
 
     pe_grad = grad_func(x, y)
+    
 
     grads_neighs = []
     for neigh in neighs:
@@ -175,7 +168,7 @@ def spring_grad(x, y, neighs, k=0.1, ideal_distance=0.5, en_func=toy_potential, 
             force_y *= -1
 
         force_spring = np.array([force_x, force_y])
-
+        
         grads_neighs.append(force_spring)
 
     # print(f"\t{grads_neighs=}")
@@ -208,61 +201,44 @@ def update_points(chain, grad, dr):
 
 
 def update_points_spring(chain, dr, en_func, grad_func, k=1, ideal_dist=0.5):
-    new_chain = [chain[0]]
-
+    chain_copy = np.zeros_like(chain)
+    chain_copy[0] = chain[0]
+    chain_copy[-1] = chain[-1]
+    
     for i in range(1, len(chain) - 1):
-
-        point = chain[i]
-        p_current = point
-
-        p_x, p_y = p_current
-        if i == 0:
-            neighs = [chain[1]]
-        elif i == len(chain) - 1:
-            neighs = [chain[-2]]
-
-        else:
-            neighs = [chain[i - 1], chain[i + 1]]
-        grad_x, grad_y = spring_grad(p_x, p_y, neighs=neighs, k=k, ideal_distance=ideal_dist, grad_func=grad_func, en_func=en_func)
+        view = chain[i-1:i+2]
+        p_x, p_y = chain[i]
+        
+        grad_x, grad_y = spring_grad(view, k=k, ideal_distance=ideal_dist, grad_func=grad_func, en_func=en_func)
+        
         # print(f"{grad_x=} {grad_y=}")
-        p_new = (p_x + (grad_x * dr), p_y + (grad_y * dr))
+        p_new = np.array((p_x + (grad_x * dr), p_y + (grad_y * dr)))
+        
+        chain_copy[i] = p_new
+    
 
-        p_current = p_new
-        new_chain.append(p_current)
-    new_chain.append(chain[-1])
-
-    return new_chain
+    return chain_copy
 
 
 def update_points_spring_neb(chain, dr, en_func, grad_func, k=1, ideal_dist=0.5):
-    new_chain = [chain[0]]
-
+    
+    chain_copy = np.zeros_like(chain)
+    chain_copy[0] = chain[0]
+    chain_copy[-1] = chain[-1]
+    
     for i in range(1, len(chain) - 1):
-
-        point = chain[i]
-        p_current = point
-
-        p_x, p_y = p_current
-        if i == 0:
-            neighs = [chain[1]]
-        elif i == len(chain) - 1:
-            neighs = [chain[-2]]
-
-        else:
-            neighs = [chain[i - 1], chain[i + 1]]
-        grad_x, grad_y = spring_grad_neb(p_x, p_y, neighs=neighs, k=k, ideal_distance=ideal_dist, grad_func=grad_func, en_func=en_func)
+        view = chain[i-1:i+2]
+        p_x, p_y = chain[i]
         
-        
-        
+        grad_x, grad_y = spring_grad_neb(view, k=k, ideal_distance=ideal_dist, grad_func=grad_func, en_func=en_func)
         
         # print(f"{grad_x=} {grad_y=}")
-        p_new = (p_x + (grad_x * dr), p_y + (grad_y * dr))
+        p_new = np.array((p_x + (grad_x * dr), p_y + (grad_y * dr)))
+        
+        chain_copy[i] = p_new
+    
 
-        p_current = p_new
-        new_chain.append(p_current)
-    new_chain.append(chain[-1])
-
-    return new_chain
+    return chain_copy
 
 
 def _check_en_converged(chain_prev, chain_new, en_func, en_thre):
@@ -283,7 +259,7 @@ def _check_grad_converged(chain_prev, chain_new, grad_func, grad_thre):
         node_prev = chain_prev[i]
         node_new = chain_new[i]
 
-        delta_grad_x, delta_grad_y = np.abs(grad_func(node_new[0], node_new[1]) - en_func(node_prev[0], node_prev[1]))
+        delta_grad_x, delta_grad_y = np.abs(grad_func(node_new[0], node_new[1]) - grad_func(node_prev[0], node_prev[1]))
         # print(f"{delta_grad_x=} {delta_grad_y=}")
 
         if (delta_grad_x > grad_thre) or (delta_grad_y > grad_thre):
@@ -298,7 +274,7 @@ def optimize_chain(chain, en_func, grad_func, update_func, k=0.1, dist_id=0.1, g
     chain_previous = chain.copy()
 
     # print(f"{chain_previous=}\n")
-
+    chain_traj = []
     chain_optimized = False
 
     nsteps = 0
@@ -311,22 +287,22 @@ def optimize_chain(chain, en_func, grad_func, update_func, k=0.1, dist_id=0.1, g
         if en_converged and grad_converged:
             chain_optimized = True
 
+        chain_traj.append(new_chain)
+            
         chain_previous = new_chain.copy()
         nsteps += 1
+        
+        
 
     if chain_optimized:
         print("Chain converged!")
     else:
         print("Chain did not converge...")
 
-    print(f"final chain (PEB): {new_chain}")
-    for i in range(len(new_chain) - 1):
-        print(f"dist {i}-{i+1}: {dist(new_chain[i], new_chain[i+1])}")
-    return new_chain
-
-
-# ### NB:
-# For top left to bottom right, k=25, idealdist=10
+    # print(f"final chain (PEB): {new_chain}")
+    # for i in range(len(new_chain) - 1):
+    #     print(f"dist {i}-{i+1}: {dist(new_chain[i], new_chain[i+1])}")
+    return new_chain, chain_traj
 
 # + tags=[]
 np.random.seed(1)
@@ -371,14 +347,14 @@ chain = np.linspace((-3.7933036307483574, -3.103697226077475), (3, 2), nimages)
 plt.plot([(point[0]) for point in chain], [(point[1]) for point in chain], "^--", c="white", label="original")
 
 
-opt_chain_peb = optimize_chain(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring, max_steps=nsteps,k=k)
+opt_chain_peb, _ = optimize_chain(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring, max_steps=nsteps,k=k)
 print(f"{opt_chain_peb=}")
 points_x = [point[0] for point in opt_chain_peb]
 points_y = [point[1] for point in opt_chain_peb]
 plt.plot(points_x, points_y, "*--", c="white", label="PEB")
 
 
-opt_chain_neb = optimize_chain(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring_neb, max_steps=nsteps,k=k)
+opt_chain_neb, _ = optimize_chain(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring_neb, max_steps=nsteps,k=k)
 print(f"{opt_chain_neb=}")
 points_x = [point[0] for point in opt_chain_neb]
 points_y = [point[1] for point in opt_chain_neb]
@@ -430,178 +406,15 @@ plt.plot(ens_peb, 'o--',label="peb")
 plt.plot(ens_orig, label="orig")
 plt.legend()
 
-# # Refactor
-#
-
-# +
-# params
-nsteps = 100
-chain = np.linspace((-3.7933036307483574, -3.103697226077475), (3, 2), nimages)
-chain=chain; en_func=en_func; grad_func=grad_func; en_thre=0.01; grad_thre=0.01; update_func=update_points_spring_neb; max_steps=nsteps;k=k;
-
-
-
-
-
-ref_chain = optimize_chain(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring_neb, max_steps=nsteps,k=k)
-print(ref_chain)
-
-
-# -
-
-def update_points_spring_neb_v2(chain, dr, en_func, grad_func, k=1, ideal_dist=0.5):
-    
-    chain_copy = np.zeros_like(chain)
-    chain_copy[0] = chain[0]
-    chain_copy[-1] = chain[-1]
-    
-    for i in range(1, len(chain) - 1):
-        view = chain[i-1:i+2]
-        p_x, p_y = chain[i]
-        
-        grad_x, grad_y = spring_grad_neb_2(view, k=k, ideal_distance=ideal_dist, grad_func=grad_func, en_func=en_func)
-        
-        # print(f"{grad_x=} {grad_y=}")
-        p_new = np.array((p_x + (grad_x * dr), p_y + (grad_y * dr)))
-        
-        chain_copy[i] = p_new
-    
-
-    return chain_copy
-
-
-def spring_grad_neb_2(view, k=0.1, ideal_distance=0.5, en_func=toy_potential, grad_func=toy_grad):
-
-    # pe_grad = 0
-    neighs = view[[0,2]]
-    x,y = view[1]
-
-    
-    vec_tan_path = neighs[1] - neighs[0]
-
-
-    unit_tan_path = vec_tan_path / np.linalg.norm(vec_tan_path)
-    
-
-    pe_grad = grad_func(x, y)
-    pe_grad_nudged_const = np.dot(pe_grad, unit_tan_path)
-    pe_grad_nudged = pe_grad - pe_grad_nudged_const * unit_tan_path
-
-    grads_neighs = []
-    for neigh in neighs:
-        neigh_x, neigh_y = neigh
-        dist_x = np.abs(neigh_x - x)
-        dist_y = np.abs(neigh_y - y)
-
-        force_x = -k * (dist_x - ideal_distance)
-        force_y = -k * (dist_y - ideal_distance)
-        # print(f"\t{force_x=} {force_y=}")
-
-        if neigh_x > x:
-            force_x *= -1
-
-        if neigh_y > y:
-            force_y *= -1
-
-        force_spring = np.array([force_x, force_y])
-        force_spring_nudged_const = np.dot(force_spring, unit_tan_path)
-        force_spring_nudged = force_spring - force_spring_nudged_const * unit_tan_path
-
-        grads_neighs.append(force_spring_nudged)
-
-    # print(f"\t{grads_neighs=}")
-
-    tot_grads_neighs = np.sum(grads_neighs, axis=0)
-
-    # print(f"{tot_grads_neighs}")
-    return tot_grads_neighs - pe_grad_nudged
-
-
-def optimize_chain_2(chain, en_func, grad_func, update_func, k=0.1, dist_id=0.1, grad_thre=0.01, en_thre=0.01, max_steps=1000):
-    chain_previous = chain.copy()
-
-    # print(f"{chain_previous=}\n")
-    chain_traj = []
-    chain_optimized = False
-
-    nsteps = 0
-
-    while nsteps < max_steps and not chain_optimized:
-        new_chain = update_func(chain=chain_previous, dr=dr, k=k, ideal_dist=dist_id, en_func=en_func, grad_func=grad_func)
-        en_converged = _check_en_converged(chain_prev=chain_previous, chain_new=new_chain, en_func=en_func, en_thre=en_thre)
-        grad_converged = _check_grad_converged(chain_prev=chain_previous, chain_new=new_chain, grad_func=grad_func, grad_thre=grad_thre)
-
-        if en_converged and grad_converged:
-            chain_optimized = True
-
-        chain_traj.append(new_chain)
-            
-        chain_previous = new_chain.copy()
-        nsteps += 1
-        
-        
-
-    # if chain_optimized:
-    #     print("Chain converged!")
-    # else:
-    #     print("Chain did not converge...")
-
-    # print(f"final chain (PEB): {new_chain}")
-    # for i in range(len(new_chain) - 1):
-    #     print(f"dist {i}-{i+1}: {dist(new_chain[i], new_chain[i+1])}")
-    return new_chain, chain_traj
-
-# + tags=[]
-# params
-chain = np.linspace((-3.7933036307483574, -3.103697226077475), (3, 2), nimages)
-chain=chain; en_func=en_func; grad_func=grad_func; en_thre=0.01; grad_thre=0.01; update_func=update_points_spring_neb_v2; max_steps=nsteps;k=k;
-
-new_chain, _ = optimize_chain_2(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring_neb_v2, max_steps=nsteps,k=k)
-print(f"new chain (PEB): {new_chain}")
-# for i in range(len(new_chain) - 1):
-#     print(f"dist {i}-{i+1}: {dist(new_chain[i], new_chain[i+1])}")
-
-# + tags=[]
-ref_chain == new_chain
-
-# +
-# set up plot for potential
-
-num = 10
-fig = 10
-f, ax = plt.subplots(figsize=(1.18 * fig, fig))
-x = np.linspace(start=-4, stop=4, num=num)
-y = x.reshape(-1, 1)
-
-
-# h = en_func(x, y)
-h = en_func(x, y)
-cs = plt.contourf(x, x, h)
-cbar = f.colorbar(cs)
-points_x = [point[0] for point in ref_chain]
-points_y = [point[1] for point in ref_chain]
-plt.plot(points_x, points_y, "o--", c="white", label="ref")
-
-
-points_x = [point[0] for point in new_chain]
-points_y = [point[1] for point in new_chain]
-plt.plot(points_x, points_y, "x--", c="white", label="new")
-
-
-plt.legend()
-print(opt_chain_neb)
-plt.show()
-# -
-
-new_chain - ref_chain
-
 # # Anim
 
 chain = np.linspace((-5, -3), (5, 3), nimages)
 
 chain
 
-final_chain, all_chains = optimize_chain_2(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring_neb_v2, max_steps=nsteps,k=k)
+# + tags=[]
+final_chain, all_chains = optimize_chain(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring_neb, max_steps=nsteps,k=k)
+final_chain_peb, all_chains_peb = optimize_chain(chain=chain, en_func=en_func, grad_func=grad_func, en_thre=0.01, grad_thre=0.01, update_func=update_points_spring, max_steps=nsteps,k=k)
 
 # +
 # %matplotlib widget
@@ -621,7 +434,7 @@ y = x.reshape(-1, 1)
 # h = en_func(x, y)
 h = en_func(x, y)
 cs = plt.contourf(x, x, h)
-cbar = f.colorbar(cs)
+cbar = f.colorbar(cs, ax=ax)
 line, = ax.plot([], [], lw=3)
 
 def init():