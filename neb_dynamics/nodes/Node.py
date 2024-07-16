3da08de2f1320951b4b6528e14567291a866849b
Jan <janestrada7@gmail.com>
2024-07-16 13:43:02 -0700

    started refactoring

--- neb_dynamics/nodes/Node.py
+++ neb_dynamics/nodes/Node.py
@@ -1,69 +1,75 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from abc import ABC, abstractmethod
+import numpy as np
+from neb_dynamics.helper_functions import RMSD
+from neb_dynamics.geodesic_interpolation.coord_utils import align_geom
+from qcio.models.structure import Structure
 
 
 @dataclass
-class AlessioError(Exception):
-    message: str
+class Node:
+    structure: Structure
 
+    converged: bool = False
+    has_molecular_graph: bool = False
+    symbols: np.array = None
 
-class Node(ABC):
-    @property
-    @abstractmethod
-    def is_a_molecule(self):
-        ...
+    def __eq__(self, other: None) -> bool:
+        return self.is_identical(other)
 
-    @property
-    @abstractmethod
-    def converged(self):
-        ...
+    def is_identical(self, other: Node,
+                     *,
+                     global_rmsd_cutoff: float = 20.0,
+                     fragment_rmsd_cutoff: float = 0.5,
+                     kcal_mol_cutoff: float = 1.0) -> bool:
+        """
+        computes whether two nodes are identical.
+        if nodes have computable molecular graphs, will check that
+        they are isomorphic to each other + that their distances are
+        within a threshold (default: 0.5 Anstroms) and their enegies
+        are within a windos (default: 1.0 kcal/mol).
+        otherwise, will only check distances.
+        """
+        if self.has_molecular_graph:
+            assert other.has_molecular_graph, "Both node objects must have computable molecular graphs."
+            conditions = [self._is_connectivity_identical(
+                other), self._is_conformer_identical(other)]
+        else:
+            conditions = [self._is_conformer_identical(other,
+                                                       global_rmsd_cutoff=global_rmsd_cutoff,
+                                                       fragment_rmsd_cutoff=fragment_rmsd_cutoff,
+                                                       kcal_mol_cutoff=kcal_mol_cutoff)]
 
-    @abstractmethod
-    def en_func(coords):
-        ...
+        return all(conditions)
 
-    @abstractmethod
-    def grad_func(coords):
-        ...
+    def _is_conformer_identical(self, other: Node,
+                                *,
+                                global_rmsd_cutoff: float = 20.0,
+                                fragment_rmsd_cutoff: float = 0.5,
+                                kcal_mol_cutoff: float = 1.0) -> bool:
 
-    @property
-    @abstractmethod
-    def energy(self):
-        ...
+        global_dist, aligned_geometry = align_geom(refgeom=other.structure.geometry, geom=self.structure.geometry)
+        per_frag_dists = []
+        if self.has_molecular_graph:
+            self_frags = split_structure_into_frags(self.structure)
+            other_frags = split_structure_into_frags(split_td_into_frags)
+            for frag_self, frag_other in zip(self_frags, other_frags):
+                aligned_frag_self = frag_self.align_to_td(frag_other)
+                frag_dist = RMSD(aligned_frag_self.coords,
+                                    frag_other.coords)[0]
+                per_frag_dists.append(frag_dist)
 
-    @abstractmethod
-    def copy(self):
-        ...
+        en_delta = np.abs((self.energy - other.energy) * 627.5)
 
-    @property
-    @abstractmethod
-    def gradient(self):
-        ...
+        global_rmsd_identical = global_dist <= global_rmsd_cutoff
+        fragment_rmsd_identical = max(
+            per_frag_dists) <= fragment_rmsd_cutoff
+        rmsd_identical = global_rmsd_identical and fragment_rmsd_identical
+        energies_identical = en_delta < kcal_mol_cutoff
+        # print(f"\nbarrier_to_conformer_rearr: {barrier} kcal/mol\n{en_delta=}\n")
 
-    @property
-    @abstractmethod
-    def coords(self):
-        ...
-
-    @property
-    @abstractmethod
-    def do_climb(self):
-        ...
-
-    @abstractmethod
-    def dot_function(self, other):
-        ...
-
-    @abstractmethod
-    def update_coords(self, coords):
-        ...
-
-    @abstractmethod
-    def do_geometry_optimization(self) -> Node:
-        ...
-
-    @abstractmethod
-    def is_identical(self, other):
-        ...
+        if rmsd_identical and energies_identical:  # and barrier_accessible:
+            return True
+        else:
+            return False
--- notebooks/automated_mechanism_generation.py
+++ notebooks/automated_mechanism_generation.py
@@ -1,4 +1,43 @@
+# +
+from qcio import ProgramInput, ProgramOutput, Structure
 
+from chemcloud import CCClient
+
+water = Structure(
+    symbols=["O", "H", "H"],
+    geometry=[
+        [0.0000, 0.00000, 0.0000],
+        [0.2774, 0.89290, 0.2544],
+        [0.6067, -0.23830, -0.7169],
+    ],
+)
+
+client = CCClient()
+
+prog_inp = ProgramInput(
+    structure=water,
+    model={"method": "b3lyp", "basis": "6-31g"},
+    calctype="gradient",  # Or "gradient" or "hessian"
+    keywords={},
+)
+future_result = client.compute("terachem", prog_inp, collect_files=True)
+prog_output: ProgramOutput = future_result.get()
+# ProgramOutput object containing all returned data
+print(prog_output.stdout)
+print(prog_output)
+# The energy value requested
+
+if prog_output.success:
+    print(prog_output.results.energy)
+    print(prog_output.files.keys())
+else:
+    print(prog_output.traceback)
+
+# -
+
+prog_inp.calctype = 'energy'
+
+prog_output.results.gradient
 
 # +
 from neb_dynamics.tdstructure import TDStructure
