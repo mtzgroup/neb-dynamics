mtzgroup/neb-dynamics

commit 45dc0cc1751c89ea48111500155d3448725914d2

Threewell potential seems to work

neb_dynamics/neb.py
@@ -4,6 +4,7 @@ import shutil
 import sys
 from dataclasses import dataclass, field
 from typing import Tuple
+from numpy.typing import NDArray
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -269,11 +270,22 @@ class NEB:
                 obj=self,
             )
 
+    def _update_cache(self, chain: Chain, gradients: NDArray, energies: NDArray) -> None:
+        """
+        will update the `_cached_energy` and `_cached_gradient` attributes in the chain
+        nodes based on the input `gradients` and `energies`
+        """
+        for node, grad, ene in zip(chain, gradients, energies):
+            node._cached_energy = ene
+            node._cached_gradient = grad
+
     def update_chain(self, chain: Chain) -> Chain:
-
-        self.engine.compute_gradients(chain)
-
         import neb_dynamics.chainhelpers as ch
+
+        grads = self.engine.compute_gradients(chain)
+        enes = self.engine.compute_energies(chain)
+        self._update_cache(chain, grads, enes)
+
         grad_step = ch.compute_NEB_gradient(chain)
         new_chain = self.optimizer.optimize_step(
             chain=chain, chain_gradients=grad_step)
@@ -281,7 +293,8 @@ class NEB:
         # need to copy the gradients from the converged nodes
         for new_node, old_node in zip(new_chain.nodes, chain.nodes):
             if old_node.converged:
-                new_node._cached_result = old_node._cached_result
+                new_node._cached_energy = old_node._cached_energy
+                new_node._cached_gradient = old_node._cached_gradient
 
         self.engine.compute_gradients(new_chain)
 

neb_dynamics/nodes/node.py
@@ -1,27 +1,112 @@
 from __future__ import annotations
 
-from pathlib import Path
 from dataclasses import dataclass
 from qcio.models.structure import Structure
 from qcio.models.outputs import ProgramOutput
 import numpy as np
+from numpy.typing import NDArray
 from neb_dynamics.molecule import Molecule
 from neb_dynamics.qcio_structure_helpers import structure_to_molecule
 from neb_dynamics.errors import GradientsNotComputedError, EnergiesNotComputedError
+from neb_dynamics.fakeoutputs import FakeQCIOResults
+from abc import ABC, abstractmethod
 
 
 @dataclass
-class Node:
-    structure: Structure
-    graph: Molecule = None
-    converged: bool = False
-    has_molecular_graph: bool = True
+class Node(ABC):
 
     do_climb: bool = False
+
+    @property
+    @abstractmethod
+    def has_molecular_graph(self): ...
+
+    @property
+    @abstractmethod
+    def converged(self): ...
+
+    @property
+    @abstractmethod
+    def structure(self): ...
+
+    @property
+    @abstractmethod
+    def _cached_energy(self): ...
+
+    @property
+    @abstractmethod
+    def _cached_gradient(self): ...
+
+    @abstractmethod
+    def update_coords(coords): ...
+
+    @abstractmethod
+    def copy(self):
+        ...
+
+    @abstractmethod
+    def coords(self):
+        ...
+
+@dataclass
+class XYNode(Node):
+    structure: np.array = None
+    converged: bool = False
+    has_molecular_graph = False
+    _cached_energy: float = None
+    _cached_gradient: NDArray = None
+
+    def update_coords(self, new_coords: np.array) -> XYNode:
+        """
+        returns a new XYNode with coordinates 'new_coords'
+        """
+        copy_node = self.copy()
+        copy_node.structure = new_coords
+        return copy_node
+
+    @property
+    def coords(self) -> np.array:
+        """
+        shortcut to coordinates stored in Structure.geometry
+        """
+        return self.structure
+
+    @property
+    def energy(self):
+        if self._cached_energy is not None:
+            return self._cached_energy
+        else:
+            raise EnergiesNotComputedError()
+
+    @property
+    def gradient(self):
+        if self._cached_gradient is not None:
+            return self._cached_gradient
+        else:
+            raise GradientsNotComputedError()
+
+    def copy(self) -> XYNode:
+        return XYNode(**self.__dict__)
+
+    def __eq__(self, other: Node) -> bool:
+        DIST_THRESH = 0.001
+        return np.linalg.norm(self.coords - other.coords) <= DIST_THRESH
+
+
+@dataclass
+class StructureNode(Node):
+    structure: Structure = None
+    has_molecular_graph: bool = True
+    converged: bool = False
+    _cached_energy: float = None
+    _cached_gradient: NDArray = None
+
     _cached_result: ProgramOutput = None
+    graph: Molecule = None
 
     def __eq__(self, other: Node) -> bool:
         from neb_dynamics.nodes.nodehelpers import is_identical
+
         return is_identical(self, other)
 
     def __post_init__(self):
@@ -41,27 +126,13 @@ class Node:
         copy_node = self.copy()
         copy_node._cached_result = None
         new_struct_dict = copy_node.structure.__dict__.copy()
-        new_struct_dict['geometry'] = new_coords
+        new_struct_dict["geometry"] = new_coords
         copy_node.structure = Structure(**new_struct_dict)
         return copy_node
-
-    def copy(self) -> Node:
-        return Node(**self.__dict__)
-
-    @property
-    def energy(self):
-        if self._cached_result is not None:
-            return self._cached_result.results.energy
-        else:
-            raise EnergiesNotComputedError
-
-    @property
-    def gradient(self):
-        if self._cached_result is not None:
-            return self._cached_result.results.gradient
-        else:
-            raise GradientsNotComputedError
 
     @property
     def symbols(self):
         return self.structure.symbols
+
+    def copy(self) -> StructureNode:
+        return StructureNode(**self.__dict__)

tests/test_all.py
@@ -9,7 +9,8 @@ from neb_dynamics.neb import NEB
 from neb_dynamics.optimizers.VPO import VelocityProjectedOptimizer
 from neb_dynamics.tdstructure import TDStructure
 from neb_dynamics.trajectory import Trajectory
-from neb_dynamics.engine import QCOPEngine
+from neb_dynamics.engine import QCOPEngine, ThreeWellPotential
+from neb_dynamics.nodes.node import XYNode
 
 from qcio.models.inputs import ProgramInput
 
@@ -164,9 +165,31 @@ def test_msmep(test_data_dir: Path = Path("/home/jdep/neb_dynamics/tests")):
     assert len(h.ordered_leaves) == 2, f"MSMEP found incorrect number of elementary steps.\
           Found {len(h.ordered_leaves)}. Reference: 2"
 
+def test_2d_neb():
+
+    nimages = 15
+
+    ### node 2d
+    end_point = (3.00002182, 1.99995542)
+    start_point = (-3.77928812, -3.28320392)
+    coords = np.linspace(start_point, end_point, nimages)
+    ks = 0.1
+    cni = ChainInputs(k=ks, delta_k=0, node_class=XYNode)
+    nbi = NEBInputs(tol=0.1, barrier_thre=5, v=True, max_steps=500, climb=False)
+    chain = Chain(nodes=[XYNode(structure=xy) for xy in coords], parameters=cni)
+    eng = ThreeWellPotential()
+    opt = VelocityProjectedOptimizer(timestep=0.001)
+    n = NEB(initial_chain=chain,
+            parameters=nbi,
+            optimizer=opt,
+            engine=eng)
+    n.optimize_chain()
+
+
 
 if __name__ == "__main__":
     # test_tdstructure()
     # test_trajectory()
     # test_neb()
-    test_msmep()
+    # test_msmep()
+    test_2d_neb()

neb_dynamics/chain.py
@@ -15,29 +15,14 @@ from neb_dynamics.errors import EnergiesNotComputedError, GradientsNotComputedEr
 from neb_dynamics.geodesic_interpolation.fileio import write_xyz
 
 
-from neb_dynamics.nodes.node import Node
+from neb_dynamics.nodes.node import Node, StructureNode
 from neb_dynamics.constants import BOHR_TO_ANGSTROMS
 from neb_dynamics.inputs import ChainInputs
+from neb_dynamics.fakeoutputs import FakeQCIOResults, FakeQCIOOutput
 from neb_dynamics.helper_functions import (
     linear_distance,
     qRMSD_distance,
 )
-
-
-@dataclass
-class FakeQCIOOutput:
-    """
-    class that has an attribute results that has another attribute `energy` and `gradient`
-    for returning previously cached properties. Class exists to add order across different
-    node types that were not created with QCIO.
-    """
-    results: FakeQCIOResults
-
-
-@dataclass
-class FakeQCIOResults:
-    energy: float
-    gradient: np.array
 
 
 @dataclass
@@ -68,7 +53,7 @@ class Chain:
 
         fp = Path(fp)
 
-        nodes = [Node(struct) for struct in read_multiple_structure_from_file(
+        nodes = [StructureNode(structure=struct) for struct in read_multiple_structure_from_file(
             fp, charge=charge, spinmult=spinmult)]
         chain = cls(nodes=nodes, parameters=parameters)
 

neb_dynamics/chainhelpers.py
@@ -114,7 +114,7 @@ def neighs_grad_func(chain: Chain, prev_node: Node, current_node: Node, next_nod
     pe_grad = current_node.gradient
 
     # remove rotations and translations
-    if pe_grad.shape[1] >= 3:  # if we have at least 3 atoms
+    if len(pe_grad.shape) > 1 and pe_grad.shape[1] >= 3:  # if we have at least 3 atoms
         pe_grad[0, :] = 0  # this atom cannot move
         pe_grad[1, :2] = 0  # this atom can only move in a line
         pe_grad[2, :1] = 0  # this atom can only move in a plane

neb_dynamics/convergence_helpers.py
@@ -95,10 +95,11 @@ def _update_node_convergence(chain: Chain, indices: np.array, prev_chain: Chain)
     endpoints_indices = [0, len(chain)-1]
     for i, (node, prev_node) in enumerate(zip(chain, prev_chain)):
         if i in indices or i in endpoints_indices:
-            if prev_node._cached_result is not None:
+            if prev_node._cached_gradient is not None:
                 # print(f"node{i} is frozen with _cached res: {prev_node._cached_result}")
                 node.converged = True
-                node._cached_result = prev_node._cached_result
+                node._cached_gradient = prev_node._cached_gradient
+                node._cached_energy = prev_node._cached_energy
         else:
             node.converged = False
 
@@ -106,4 +107,5 @@ def _update_node_convergence(chain: Chain, indices: np.array, prev_chain: Chain)
 def _copy_node_information_to_converged(new_chain: Chain, old_chain: Chain) -> None:
     for new_node, old_node in zip(new_chain.nodes, old_chain.nodes):
         if old_node.converged:
-            new_node._cached_result = old_node._cached_result
+            new_node._cached_gradient = old_node._cached_gradient
+            new_node._cached_energy = old_node._cached_energy

neb_dynamics/elementarystep.py
@@ -105,7 +105,8 @@ def check_if_elem_step(inp_chain: Chain, engine: Engine) -> ElemStepResults:
     return ElemStepResults(is_elem_step=elem_step,
                            is_concave=concavity_results.is_concave,
                            splitting_criterion='maxima',
-                           minimization_results=[pseu_irc_results.found_reactant, pseu_irc_results.found_product],
+                           minimization_results=[
+                               pseu_irc_results.found_reactant, pseu_irc_results.found_product],
                            number_grad_calls=n_geom_opt_grad_calls
                            )
 
@@ -144,8 +145,10 @@ def is_approx_elem_step(chain: Chain, engine: Engine, slope_thresh=0.1) -> Tuple
     # isomorphic to each other. Otherwise, we will decide only based on distance.
     # (which is bad!!)
     if nodes_have_graph:
-        r_passes = r_passes_opt and _is_connectivity_identical(r_traj[-1], chain[0])
-        p_passes = p_passes_opt and _is_connectivity_identical(p_traj[-1], chain[-1])
+        r_passes = r_passes_opt and _is_connectivity_identical(
+            r_traj[-1], chain[0])
+        p_passes = p_passes_opt and _is_connectivity_identical(
+            p_traj[-1], chain[-1])
     else:
         r_passes = r_passes_opt
         p_passes = p_passes_opt
@@ -211,6 +214,20 @@ def _distances_to_refs(ref1: Node, ref2: Node, raw_node: Node) -> List[float]:
     return [dist_to_ref1, dist_to_ref2]
 
 
+def _run_geom_opt(node: Node, engine: Engine):
+    """
+    will run a check on whether the Engine has implemented the
+    geometry optimization function. If not, it will just run Steepest
+    Descent.
+    """
+    try:
+        opt_traj = engine.compute_geometry_optimization(node)
+    except AttributeError:
+        opt_traj = engine.steepest_descent(node, max_steps=500, ss=.001)
+
+    return opt_traj
+
+
 def _chain_is_concave(chain: Chain, engine: Engine) -> ConcavityResults:
     """
     will assess+categorize the presence of minima on the chain.
@@ -224,7 +241,7 @@ def _chain_is_concave(chain: Chain, engine: Engine) -> ConcavityResults:
         minimas_is_r_or_p = []
         try:
             for i in ind_minima:
-                opt_traj = engine.compute_geometry_optimization(chain[i])
+                opt_traj = _run_geom_opt(chain[i], engine=engine)
                 n_grad_calls += len(opt_traj)
                 opt = opt_traj[-1]
                 opt_results.append(opt)
@@ -281,11 +298,12 @@ def pseudo_irc(chain: Chain, engine: Engine):
 
         candidate_r = chain[arg_max - 1]
         candidate_p = chain[arg_max + 1]
-        r_traj = engine.compute_geometry_optimization(candidate_r)
+
+        r_traj = _run_geom_opt(candidate_r, engine=engine)
         r = r_traj[-1]
         n_grad_calls += len(r_traj)
 
-        p_traj = engine.compute_geometry_optimization(candidate_p)
+        p_traj = _run_geom_opt(candidate_p, engine=engine)
         n_grad_calls += len(p_traj)
         p = p_traj[-1]
 

neb_dynamics/engine.py
@@ -11,15 +11,18 @@ from qcio.models.inputs import ProgramInput, DualProgramInput, Structure
 import qcop
 import concurrent
 
-from neb_dynamics.nodes.node import Node
+from neb_dynamics.nodes.node import Node, StructureNode
+from neb_dynamics.errors import EnergiesNotComputedError, GradientsNotComputedError
 from neb_dynamics.qcio_structure_helpers import _change_prog_input_property
+from neb_dynamics.fakeoutputs import FakeQCIOOutput
+from qcio.models.outputs import ProgramOutput
 
 
 @dataclass
 class Engine(ABC):
 
     @abstractmethod
-    def compute_gradients(self, chain: Chain) -> NDArray:
+    def compute_gradients(self, chain: Union[Chain, List]) -> Union[FakeQCIOOutput, ProgramOutput]:
         """
         returns the gradients for each node in the chain as
         specified by the object inputs
@@ -27,25 +30,40 @@ class Engine(ABC):
         ...
 
     @abstractmethod
-    def compute_energies(self, chain: Chain) -> NDArray:
+    def compute_energies(self, chain: Union[Chain, List]) -> Union[FakeQCIOOutput, ProgramOutput]:
         """
         returns the gradients for each node in the chain as
         specified by the object inputs
         """
         ...
 
-    def steepest_descent(self, node: Node, ss=1, max_steps=10) -> list[Node]:
+    def steepest_descent(self, node: Node, ss=1, max_steps=10,
+                         ene_thre: float = 1e-6, grad_thre: float = 1e-4) -> list[Node]:
         history = []
         last_node = node.copy()
         # make sure the node isn't frozen so it returns a gradient
         last_node.converged = False
-        for i in range(max_steps):
+
+        curr_step = 0
+        converged = False
+        while curr_step < max_steps and not converged:
             grad = last_node.gradient
             new_coords = last_node.coords - 1*ss*grad
             node_new = last_node.update_coords(new_coords)
-            self.compute_gradients([node_new])
+            grads = self.compute_gradients([node_new])
+            ene = self.compute_energies([node_new])
+            node_new._cached_gradient = grads[0]
+            node_new._cached_energy = ene[0]
+
             history.append(node_new)
+
+            delta_en = node_new.energy - last_node.energy
+            grad_inf_norm = np.amax(np.abs(node_new.gradient))
+            converged = delta_en <= ene_thre and grad_inf_norm <= grad_thre
+
             last_node = node_new.copy()
+            curr_step += 1
+
         return history
 
 
@@ -55,20 +73,20 @@ class QCOPEngine(Engine):
     program: str
     geometry_optimizer: str = 'geometric'
 
-    def _run_calc(self, calctype: str, chain: Union[Chain, List]) -> List[Node]:
+    def _run_calc(self, calctype: str, chain: Union[Chain, List]) -> List[StructureNode]:
         if isinstance(chain, Chain):
             assert isinstance(
-                chain.nodes[0], Node), "input Chain has nodes incompatible with QCOPEngine."
+                chain.nodes[0], StructureNode), "input Chain has nodes incompatible with QCOPEngine."
             node_list = chain.nodes
         elif isinstance(chain, list):
             assert isinstance(
-                chain[0], Node), "input list has nodes incompatible with QCOPEngine."
+                chain[0], StructureNode), "input list has nodes incompatible with QCOPEngine."
             node_list = chain
         else:
             raise ValueError(
                 f"Input needs to be a Chain or a List. You input a: {type(chain)}")
 
-        # first make sure the program input has calctype set to gradients
+        # first make sure the program input has calctype set to input calctype
         prog_inp = _change_prog_input_property(
             prog_inp=self.program_input, key='calctype', value=calctype)
 
@@ -108,12 +126,18 @@ class QCOPEngine(Engine):
         return node_list
 
     def compute_gradients(self, chain: Union[Chain, List]) -> NDArray:
-        node_list = self._run_calc(chain=chain, calctype='gradient')
-        return np.array([node.gradient for node in node_list])
+        try:
+            return chain.gradients
+        except GradientsNotComputedError:
+            node_list = self._run_calc(chain=chain, calctype='gradient')
+            return np.array([node.gradient for node in node_list])
 
     def compute_energies(self, chain: Chain) -> NDArray:
-        node_list = self._run_calc(chain=chain, calctype='energy')
-        return np.array([node.energy for node in node_list])
+        try:
+            return chain.energies
+        except EnergiesNotComputedError:
+            node_list = self._run_calc(chain=chain, calctype='energy')
+            return np.array([node.energy for node in node_list])
 
     def _run_geom_opt_calc(self, structure: Structure):
         """
@@ -132,7 +156,7 @@ class QCOPEngine(Engine):
             self.geometry_optimizer, dpi, propagate_wfn=False, rm_scratch_dir=False)
         return output
 
-    def compute_geometry_optimization(self, node: Node) -> list[Node]:
+    def compute_geometry_optimization(self, node: StructureNode) -> list[StructureNode]:
         """
         will run a geometry optimization call and parse the output into
         a list of Node objects
@@ -140,4 +164,40 @@ class QCOPEngine(Engine):
         output = self._run_geom_opt_calc(structure=node.structure)
         all_outputs = output.results.trajectory
         structures = [output.input_data.structure for output in all_outputs]
-        return [Node(structure=struct, _cached_result=result) for struct, result in zip(structures, all_outputs)]
+        return [StructureNode(structure=struct, _cached_result=result) for struct, result in zip(structures, all_outputs)]
+
+
+@dataclass
+class ThreeWellPotential(Engine):
+
+    def _en_func(self, xy: np.array) -> float:
+        """
+        computes energy from xy point
+        """
+        x, y = xy
+        return (x**2 + y - 11) ** 2 + (x + y**2 - 7) ** 2
+
+    def _grad_func(self, xy: np.array) -> NDArray:
+        """
+        computes gradient from xy point
+        """
+        x, y = xy
+        dx = 2 * (x**2 + y - 11) * (2 * x) + 2 * (x + y**2 - 7)
+        dy = 2 * (x**2 + y - 11) + 2 * (x + y**2 - 7) * (2 * y)
+        return np.array([dx, dy])
+
+    def compute_energies(self, chain: Union[Chain, List[Node]]) -> NDArray:
+        if isinstance(chain, Chain):
+            return np.array([self._en_func(xy) for xy in chain.coordinates])
+        elif isinstance(chain, List):
+            return np.array([self._en_func(xy.structure) for xy in chain])
+        else:
+            raise ValueError(f"Unsupported type {type(chain)}")
+
+    def compute_gradients(self, chain: Union[Chain, List[Node]]) -> NDArray:
+        if isinstance(chain, Chain):
+            return np.array([self._grad_func(xy) for xy in chain.coordinates])
+        elif isinstance(chain, List):
+            return np.array([self._grad_func(xy.structure) for xy in chain])
+        else:
+            raise ValueError(f"Unsupported type {type(chain)}")

commit b70d69940530f1d722c11e3e0b7ac9d00a7e6f49

fixed bug introduced in commit 45dc0cc

neb_dynamics/engines/qcop.py
@@ -109,14 +109,14 @@ class QCOPEngine(Engine):
 
     def compute_gradients(self, chain: Union[Chain, List]) -> NDArray:
         try:
-            return chain.gradients
+            return np.array([node.gradient for node in chain])
         except GradientsNotComputedError:
             node_list = self._run_calc(chain=chain, calctype="gradient")
             return np.array([node.gradient for node in node_list])
 
     def compute_energies(self, chain: Chain) -> NDArray:
         try:
-            return chain.energies
+            return np.array([node.energy for node in chain])
         except EnergiesNotComputedError:
             node_list = self._run_calc(chain=chain, calctype="energy")
             return np.array([node.energy for node in node_list])
@@ -137,7 +137,7 @@ class QCOPEngine(Engine):
         )
 
         output = self.compute_func(
-            self.geometry_optimizer, dpi, propagate_wfn=False, rm_scratch_dir=False
+            self.geometry_optimizer, dpi
         )
         return output
 

neb_dynamics/neb.py
@@ -275,7 +275,11 @@ class NEB:
         will update the `_cached_energy` and `_cached_gradient` attributes in the chain
         nodes based on the input `gradients` and `energies`
         """
+        from neb_dynamics.fakeoutputs import FakeQCIOOutput, FakeQCIOResults
         for node, grad, ene in zip(chain, gradients, energies):
+            res = FakeQCIOResults(energy=ene, gradient=grad)
+            outp = FakeQCIOOutput(results=res)
+            node._cached_result = outp
             node._cached_energy = ene
             node._cached_gradient = grad
 
@@ -289,10 +293,10 @@ class NEB:
         grad_step = ch.compute_NEB_gradient(chain)
         new_chain = self.optimizer.optimize_step(
             chain=chain, chain_gradients=grad_step)
-
         # need to copy the gradients from the converged nodes
         for new_node, old_node in zip(new_chain.nodes, chain.nodes):
             if old_node.converged:
+                new_node._cached_result = old_node._cached_result
                 new_node._cached_energy = old_node._cached_energy
                 new_node._cached_gradient = old_node._cached_gradient
 

neb_dynamics/nodes/node.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from qcio.models.structure import Structure
-from qcio.models.outputs import ProgramOutput
+from typing import Union
+
 import numpy as np
 from numpy.typing import NDArray
+from qcio.models.outputs import ProgramOutput
+from qcio.models.structure import Structure
+
+from neb_dynamics.errors import (EnergiesNotComputedError,
+                                 GradientsNotComputedError)
+from neb_dynamics.fakeoutputs import FakeQCIOOutput
 from neb_dynamics.molecule import Molecule
 from neb_dynamics.qcio_structure_helpers import structure_to_molecule
-from neb_dynamics.errors import GradientsNotComputedError, EnergiesNotComputedError
-from abc import ABC, abstractmethod
 
 
 @dataclass
@@ -67,6 +72,7 @@ class XYNode(Node):
     structure: np.array = None
     converged: bool = False
     has_molecular_graph = False
+    _cached_result: FakeQCIOOutput = None
     _cached_energy: float = None
     _cached_gradient: NDArray = None
 
@@ -101,7 +107,7 @@ class StructureNode(Node):
     _cached_energy: float = None
     _cached_gradient: NDArray = None
 
-    _cached_result: ProgramOutput = None
+    _cached_result: Union[ProgramOutput, FakeQCIOOutput] = None
     graph: Molecule = None
 
     def __eq__(self, other: Node) -> bool:
@@ -128,6 +134,9 @@ class StructureNode(Node):
         """
         copy_node = self.copy()
         copy_node._cached_result = None
+        copy_node._cached_gradient = None
+        copy_node._cached_energy = None
+
         new_struct_dict = copy_node.structure.__dict__.copy()
         new_struct_dict["geometry"] = new_coords
         copy_node.structure = Structure(**new_struct_dict)

scripts/create_msmep_from_endpoints.py
@@ -15,7 +15,7 @@ from neb_dynamics.neb import NEB, NoneConvergedException
 # from neb_dynamics.nodes.node3d_water import Node3D_Water
 # from neb_dynamics.nodes.Node3D_TC_TCPB import Node3D_TC_TCPB
 from neb_dynamics.optimizers.VPO import VelocityProjectedOptimizer
-from neb_dynamics.engine import QCOPEngine
+from neb_dynamics.engines import QCOPEngine
 
 
 def read_single_arguments():

tests/test_all.py
@@ -77,7 +77,7 @@ def test_engine():
     geom0_after = c[0].structure.geometry
     geom1_after = c[1].structure.geometry
 
-    assert np.array_equal(geom0_before and geom0_after) and np.array_equal(
+    assert np.array_equal(geom0_before, geom0_after) and np.array_equal(
         geom1_before, geom1_after), "Engine is overwriting structures of chain."
 
 
@@ -214,6 +214,7 @@ def test_2d_neb():
 if __name__ == "__main__":
     # test_tdstructure()
     # test_trajectory()
+    test_engine()
     # test_neb()
-    # test_msmep()
+    test_msmep()
     test_2d_neb()