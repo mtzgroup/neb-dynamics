8b31e31bfdff8a9efca70dabe84aaa631d5f9408
Jan <janestrada7@gmail.com>
2024-12-12 17:10:27 -0800

    updated cli

--- neb_dynamics/scripts/main_cli.py
+++ neb_dynamics/scripts/main_cli.py
@@ -13,6 +13,7 @@ from neb_dynamics.nodes.node import StructureNode
 from neb_dynamics.chain import Chain
 from neb_dynamics.msmep import MSMEP
 from neb_dynamics.nodes.nodehelpers import displace_by_dr
+from neb_dynamics.qcio_structure_helpers import read_multiple_structure_from_file
 
 
 ob_log_handler = openbabel.OBMessageHandler()
@@ -23,8 +24,12 @@ app = typer.Typer()
 
 @app.command()
 def run(
-        start: Annotated[str, typer.Argument(help='path to start file, or a reactant smiles')],
-        end: Annotated[str, typer.Argument(help='path to end file, or a product smiles')],
+        start: Annotated[str, typer.Option(
+            help='path to start file, or a reactant smiles')] = None,
+        end: Annotated[str, typer.Option(
+            help='path to end file, or a product smiles')] = None,
+        geometries:  Annotated[str, typer.Argument(help='file containing an approximate path between two endpoints. \
+            Use this if you have precompted a path you want to use. Do not use this with smiles.')] = None,
         inputs: Annotated[str, typer.Option("--inputs", "-i",
                                             help='path to RunInputs toml file')] = None,
         use_smiles: bool = False,
@@ -52,16 +57,21 @@ def run(
         end_structure = align_structures(
             start_structure, end_structure, distance_metric='RMSD')
     else:
-        start_structure = Structure.open(start)
-        end_structure = Structure.open(end)
+        if geometries is not None:
+            all_structs = read_multiple_structure_from_file(geometries)
+        elif start is not None and end is not None:
 
-    s_dict = start_structure.model_dump()
-    s_dict["charge"], s_dict["multiplicity"] = charge, multiplicity
-    start_structure = Structure(**s_dict)
+            start_structure = Structure.open(start)
+            end_structure = Structure.open(end)
+            all_structs = [start_structure, end_structure]
+        else:
+            raise ValueError(
+                "Either 'geometries' or 'start' and 'end' flags must be populated!")
 
-    e_dict = end_structure.model_dump()
-    e_dict["charge"], e_dict["multiplicity"] = charge, multiplicity
-    end_structure = Structure(**e_dict)
+        for i, s in enumerate(all_structs):
+            s_dict = s.model_dump()
+            s_dict["charge"], s_dict["multiplicity"] = charge, multiplicity
+            all_structs[i] = Structure(**s_dict)
 
     # load the RunInputs
     if inputs is not None:
@@ -72,22 +82,21 @@ def run(
     sys.stdout.flush()
 
     # minimize endpoints if requested
+    all_nodes = [StructureNode(structure=s) for s in all_structs]
     if minimize_ends:
         print("Minimizing input endpoints...")
         start_tr = program_input.engine.compute_geometry_optimization(
-            StructureNode(structure=start_structure))
-        start_node = start_tr[-1]
+            StructureNode(structure=all_structs[0]))
+
+        all_nodes[0] = start_tr[-1]
         end_tr = program_input.engine.compute_geometry_optimization(
-            StructureNode(structure=end_structure))
-        end_node = end_tr[-1]
+            StructureNode(structure=all_structs[-1]))
+        all_nodes[-1] = end_tr[-1]
         print("Done!")
-    else:
-        start_node = StructureNode(structure=start_structure)
-        end_node = StructureNode(structure=end_structure)
 
     # create Chain
     chain = Chain(
-        nodes=[start_node, end_node],
+        nodes=all_nodes,
         parameters=program_input.chain_inputs,
     )
 
@@ -101,7 +110,7 @@ def run(
 
         leaves_nebs = [
             obj for obj in history.get_optimization_history() if obj]
-        fp = Path(start)
+        fp = Path("mep_output")
         data_dir = Path(os.getcwd())
 
         if name is not None:
@@ -129,7 +138,7 @@ def run(
         print(f"RUNNING REGULAR {program_input.path_min_method}")
 
         n, elem_step_results = m.run_minimize_chain(input_chain=chain)
-        fp = Path(start)
+        fp = Path("mep_output")
         data_dir = Path(os.getcwd())
         if name is not None:
             filename = data_dir / (name + ".xyz")
@@ -145,7 +154,7 @@ def run(
     print(f"***Walltime: {end_time - start_time} s")
 
 
-@app.command()
+@ app.command()
 def ts(
     geometry: Annotated[str, typer.Argument(help='path to geometry file tpo optimize')],
     inputs: Annotated[str, typer.Option("--inputs", "-i",
@@ -192,7 +201,7 @@ def ts(
     print("Done!")
 
 
-@app.command()
+@ app.command()
 def pseuirc(geometry: Annotated[str, typer.Argument(help='path to geometry file tpo optimize')],
             inputs: Annotated[str, typer.Option("--inputs", "-i",
                                                 help='path to RunInputs toml file')] = None,
@@ -251,7 +260,7 @@ def pseuirc(geometry: Annotated[str, typer.Argument(help='path to geometry file 
     print("Done!")
 
 
-@app.command()
+@ app.command()
 def make_default_inputs(
         name: Annotated[str, typer.Option(
             "--name", help='path to output toml file')] = None,
